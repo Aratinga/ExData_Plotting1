a6b8c801a5cda7a24347621879c545f61fce6cc9
Aratinga <aratinga77@gmail.com>
2015-02-02 16:05:06 -0500

    Plot 2 code, including new file read

---
+++ plot2.R
@@ -0,0 +1,6 @@
+# plot2.R
+library("lubridate", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+power2 <- read.table("household_power_consumption.txt", header=TRUE, sep=";", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings="?")
+power2$dt <- strptime(paste(power2$Date,power2$Time),"%d/%m/%Y %R")         ## Nice column of dt from these character strings - much easier than whatever I did last time
+##### newpower2 <- subset(power2, dt >= "2007-02-01" & dt <= "2007-02-02") THIS DOES NOT GET WHAT YOU THINK! Check the tail()
+newpower2 <- subset(power2, dt >= "2007-02-01" & dt < "2007-02-03") 
