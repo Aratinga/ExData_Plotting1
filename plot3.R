569f103b8e27a514a651e451634fb52d96da44f7
Aratinga <aratinga77@gmail.com>
2015-02-03 14:42:01 -0500

    Plot 3 script
    Uses png() to create file as export in R Studio messed up the Legend
    text.

--- plot3.R
+++ plot3.R
@@ -6,10 +6,12 @@ power2$dt <- strptime(paste(power2$Date,power2$Time),"%d/%m/%Y %R")         ## N
 newpower2 <- subset(power2, dt >= "2007-02-01" & dt < "2007-02-03") 
 # set some margin parameters
 # start here if you already have the object
+png("myplot.png", width=480, height=480, units="px", pointsize = 8, type = "quartz")
 par(mar=c(4,5,1,1))
 
 plot(newpower2$dt,newpower2$Sub_metering_1,  type="n", xlab="", ylab = "Energy sub metering", col="black", ylim= c(0,40))  # I threw in this limit because I don't like hanging spikes
 points(newpower2$dt,newpower2$Sub_metering_1,  type="l", col="black" )
 points(newpower2$dt,newpower2$Sub_metering_2,  type="l", col="red" )
 points(newpower2$dt,newpower2$Sub_metering_3,  type="l", col="blue" )
-legend("topright", lwd = 2, col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lwd = 2, col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),text.font = 2)
+dev.off()
